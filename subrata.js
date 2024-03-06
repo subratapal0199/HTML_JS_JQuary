885434b72d43454adf3281c85d83fbc775cf0893
Subrata Pal <subratapal0199@gmail.com>
2024-03-06 19:43:07 +0530

    All file

---
+++ subrata.js
@@ -0,0 +1,34 @@
+// let a;
+// function getdata1() {
+//     return "My function";               //asynchronous process
+// }
+// a = getdata1();
+// console.log(a);
+
+// function myfun(){
+//     return a+b
+// }
+
+console.log("Asynchronous ways:")
+let q = new Promise((resolve,reject)=>{
+    resolve("Expected Data1");              
+});
+console.log(q);
+
+
+let pro = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        reject(new Error("unable......."));                
+    },2000);
+    
+});
+pro.then((res)=>{
+    console.log(res);
+})
+.catch((err)=>{
+    console.log(err);
+});
+
+
+
+
